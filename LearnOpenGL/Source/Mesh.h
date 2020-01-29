26bfe41e48300cf91fe3dd0f59c217778e2fbc56
Jacob Pigg <jacpigg@uat.edu>
2020-01-29 12:50:21 -0600

    Create mesh class which can hold all the data needed for model loading

---
+++ LearnOpenGL/Source/Mesh.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <vector>
+#include <glm/glm.hpp>
+#include <string>
+#include "Shader.h"
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texCoords;
+};
+
+struct Texture
+{
+	unsigned int id;
+	std::string type;
+};
+
+class Mesh
+{
+public:
+	// Mesh Data
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	// Functions
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+	void Draw(Shader shader);
+private:
+	// Render data
+	unsigned int vao{}, vbo{}, ebo{};
+
+	// Functions
+	void SetupMesh();
+};
